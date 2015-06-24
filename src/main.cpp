9bb280cd7e4c984d4b6b791cd147351811d01c0d
felixbarring <felix_barring@hotmail.com>
2015-06-24 16:37:54 +0200

    Using CMAKE

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "readingFile/readingFileTest.cpp"
+
+int main()
+{
+	std::cout<<"Learning C++\n";
+	test();
+}
--- src/readingFile/readingFileTest.cpp
+++ src/readingFile/readingFileTest.cpp
@@ -4,12 +4,12 @@
 
 using namespace std;
 
-int main () {
+void test() {
 
 	vector<string> list;
 	ifstream in_stream;
 	string line;
-	in_stream.open("hello/hello.txt");
+	in_stream.open("../resources/hello/hello.txt");
 
 	// Add all lines to the vector
 	for (string line; getline( in_stream, line ); ) {
